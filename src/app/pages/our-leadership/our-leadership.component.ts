77f929f3f5f2b13e67ca6db5468488912f6a737a
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-10-20 01:51:33 +0300

    add carousel component

--- src/app/pages/our-leadership/our-leadership.component.ts
+++ src/app/pages/our-leadership/our-leadership.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {leaderships} from "../../data";
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-our-leadership',
   templateUrl: './our-leadership.component.html',
-  styleUrls: ['./our-leadership.component.sass']
+  styleUrls: ['./our-leadership.component.sass'],
+  providers: [NgbCarouselConfig]
 })
 export class OurLeadershipComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  ourLeadership;
+  showNavigationArrows = true;
+  showNavigationIndicators = false;
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = false;
   }
 
+  ngOnInit() {
+    this.ourLeadership = leaderships;
+  }
 }
