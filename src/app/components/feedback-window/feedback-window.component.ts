f1bfbfcbde091567bd60f7e395db4252225c8fcc
Polina Zaprudskaya <pzaprudskaya@mail.ru>
2019-10-20 22:58:04 +0300

    add component "feedback"

---
+++ src/app/components/feedback-window/feedback-window.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-window',
+  templateUrl: './feedback-window.component.html',
+  styleUrls: ['./feedback-window.component.sass']
+})
+export class FeedbackWindowComponent implements OnInit {
+
+  constructor() {}
+  ngOnInit() {}
+
+}
