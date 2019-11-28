4eaed99821cdf119b4b47def7013fd16f92d1335
pzaprudskaya <pzaprudskaya@mail.ru>
2019-11-28 21:39:44 +0300

    some changes

--- src/app/shared/models/contacts-page.model.ts
+++ src/app/shared/models/contacts-page.model.ts
@@ -1,27 +1,11 @@
-interface Address {
-  type: string;
-  name: string;
-}
-interface Phone {
-  type: string;
-  name: string;
-}
-interface Fax {
-  type: string;
-  name: string;
-}
-interface Email {
-  type: string;
-  name: string;
-}
 export interface IContact {
   _id?: string;
   name: string;
   image: string;
-  numbers: Phone[];
-  addresses: Address[];
-  faxes: Fax[];
-  emails: Email[];
+  numbers: string;
+  addresses: string;
+  faxes: string;
+  emails: string;
 }
 
 export interface IAllContacts {
